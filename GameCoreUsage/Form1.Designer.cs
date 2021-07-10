d6f5b1a511235c6c8dbdc26f59396c01d3f2bc98
VincentP <vincentpote@hotmail.com>
2021-07-10 16:49:11 +0200

    winforms read frametime

--- GameCoreUsage/Form1.Designer.cs
+++ GameCoreUsage/Form1.Designer.cs
@@ -32,11 +32,17 @@ namespace GameCoreUsage
             this.button1 = new System.Windows.Forms.Button();
             this.tbLog = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.tbFrametime = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbFPS = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(84, 94);
+            this.button1.Location = new System.Drawing.Point(12, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(118, 23);
             this.button1.TabIndex = 0;
@@ -54,7 +60,7 @@ namespace GameCoreUsage
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(298, 94);
+            this.button2.Location = new System.Drawing.Point(12, 41);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(118, 23);
             this.button2.TabIndex = 2;
@@ -62,11 +68,69 @@ namespace GameCoreUsage
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // tbFrametime
+            // 
+            this.tbFrametime.Location = new System.Drawing.Point(342, 12);
+            this.tbFrametime.Name = "tbFrametime";
+            this.tbFrametime.Size = new System.Drawing.Size(100, 23);
+            this.tbFrametime.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(222, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Avg frametime";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "FPS";
+            // 
+            // tbFPS
+            // 
+            this.tbFPS.Location = new System.Drawing.Point(342, 42);
+            this.tbFPS.Name = "tbFPS";
+            this.tbFPS.Size = new System.Drawing.Size(100, 23);
+            this.tbFPS.TabIndex = 6;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(13, 92);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(117, 26);
+            this.btnStart.TabIndex = 7;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(12, 124);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(117, 26);
+            this.btnStop.TabIndex = 8;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(907, 616);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.tbFPS);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbFrametime);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.tbLog);
             this.Controls.Add(this.button1);
@@ -82,6 +146,13 @@ namespace GameCoreUsage
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox tbLog;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbFrametime;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbFPS;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button btnStop;
     }
 }
 
--- GameCoreUsage/Form1.cs
+++ GameCoreUsage/Form1.cs
@@ -9,6 +9,7 @@ using System.IO.MemoryMappedFiles;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,6 +17,9 @@ namespace GameCoreUsage
 {
     public partial class Form1 : Form
     {
+        private const int FRAMETIMES_INT_SIZE = 5000;
+        private readonly long intSize = Marshal.SizeOf(typeof(int));
+
         public Form1()
         {
             InitializeComponent();
@@ -43,7 +47,7 @@ namespace GameCoreUsage
                 //Log(shmIndexPath);
                 //using (var mmf = MemoryMappedFile.CreateFromFile(System.IO.Path.GetFullPath(shmIndexPath), FileMode.Open, "SHMINDEX", 0, MemoryMappedFileAccess.Read))
                 using (var mmf = MemoryMappedFile.OpenExisting("SHMINDEX"))
-                using (var accessor = mmf.CreateViewAccessor(0, Marshal.SizeOf(typeof(int))))
+                using (var accessor = mmf.CreateViewAccessor(0, intSize))
                 {
                     int bla;
                     accessor.Read(0, out bla);
@@ -59,10 +63,78 @@ namespace GameCoreUsage
             }
         }
 
+        //continue frametime
+        private void ContinuouslyReadFrametime()
+        {
+            using (var mmfIndex = MemoryMappedFile.OpenExisting("SHMINDEX"))
+            using (var accessorIndex = mmfIndex.CreateViewAccessor(0, Marshal.SizeOf(typeof(int))))
+            using (var mmfFrametimes = MemoryMappedFile.OpenExisting("SHMFRAMETIMES"))
+            using (var accessorFrametimes = mmfFrametimes.CreateViewAccessor(0, FRAMETIMES_INT_SIZE*intSize))
+            {
+                int ownIndex = 0;
+                while (!token.IsCancellationRequested)
+                {                    
+                    int index;
+                    accessorIndex.Read(0, out index);
+                    Log("Index is: " + index);
+
+                    int frametime;
+                    int total = 0;
+                    int count = 0;
+                    for (int j = ownIndex; j % FRAMETIMES_INT_SIZE != index; j++)
+                    {
+                        accessorFrametimes.Read((j % FRAMETIMES_INT_SIZE)*intSize, out frametime);
+                        count++;
+                        total += frametime;                        
+                    }
+
+                    if (count > 0)
+                    {
+                        double avg = ((double)total / (double)count) / 1000.00;
+                        UpdateTextbox(tbFrametime, avg.ToString());
+                    }
+                    ownIndex = index;
+                    Thread.Sleep(200);
+                }
+            }
+        }
+
+        private void UpdateTextbox(TextBox tb, string text)
+        {
+            if (tb.InvokeRequired)
+            {
+                tb.Invoke(new Action<TextBox, string>(UpdateTextbox), tb, text);
+                return;
+            }
+            tb.Text = text;
+        }
+
         private void Log(string text)
         {
+            if(tbLog.InvokeRequired)
+            {
+                tbLog.Invoke(new Action<string>(Log), text);
+                return;
+            }
             tbLog.AppendText(text);
             tbLog.AppendText(Environment.NewLine);
+        }
+
+        private CancellationTokenSource source = new CancellationTokenSource();
+        private CancellationToken token;
+        
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            token = source.Token;
+            Task.Factory.StartNew(() =>
+            {
+                ContinuouslyReadFrametime();
+            }, token);
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            source.Cancel();
         }
     }
 }
